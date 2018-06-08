7892cedb3752a8f62306e53ab6a60f47a59166d9
dishubagga <dishubagga9111@gmail.com>
2018-06-08 17:42:33 +0530

    recipe show and added model recipes

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-
+import{ Recipe } from '../recipe.model';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[]=[new Recipe("this is test","only for test","https://cdn.pixabay.com/photo/2017/07/16/10/43/recipe-2508859_960_720.jpg")];
+  
   constructor() { }
 
   ngOnInit() {
