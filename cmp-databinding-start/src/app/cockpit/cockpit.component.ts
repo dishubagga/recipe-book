e10a7cff046c7edb37c19aeec0d4bc49c94752dd
dishubagga <dishubagga9111@gmail.com>
2018-07-04 16:51:24 +0530

    communication between different components

---
+++ cmp-databinding-start/src/app/cockpit/cockpit.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+
+
+@Component({
+  selector: 'app-cockpit',
+  templateUrl: './cockpit.component.html',
+  styleUrls: ['./cockpit.component.css']
+})
+export class CockpitComponent implements OnInit {
+  @Output() serverCreated = new EventEmitter<{serverName:string, serverContent:string}>();
+  @Output() blueprintCreated = new EventEmitter<{serverName:string, serverContent:string}>();
+  constructor() { }
+
+  ngOnInit() {
+  }
+  newServerName = "";
+  newServerContent = "";
+
+  onAddServer(){
+    this.serverCreated.emit({serverName:this.newServerName, serverContent:this.newServerContent});
+    // this.serverElements.push({
+    //   type: "server",
+    //   name: this.newServerName,
+    //   content: this.newServerContent
+    // })
+  }
+  onAddBlueprint(){
+    this.blueprintCreated.emit({serverName:this.newServerName, serverContent:this.newServerContent});
+    // this.serverElements.push({
+    //   type:"blueprint",
+    //   name:this.newServerName,
+    //   content:this.newServerContent
+    // })
+  }
+}
